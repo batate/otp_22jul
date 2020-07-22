3b34fd8ef07421edf61db6ddd4f6c368accea311
Herminio Torres <herminiocesar@gmail.com>
2020-07-22 14:55:30 -0300

    add the boundry

---
+++ increasly/lib/increasly/boundry.ex
@@ -0,0 +1,25 @@
+defmodule Increasly.Boundry do
+  alias Increasly.Core
+
+  def start(init) do
+    spawn(fn ->
+      Core.counter(init) |> run
+    end)
+  end
+
+  def run(count) do
+    count
+    |> listen
+    |> run
+  end
+
+  def listen(count) do
+    receive do
+      :inc -> Core.up(count)
+      :dec -> Core.down(count)
+      {:state, from} ->
+        send(from, count)
+        count
+    end
+  end
+end
