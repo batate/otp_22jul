e9809056dcf5de9a5a708f78f444efef8e2090b4
Patrick Carver <patrickcarver@gmail.com>
2020-07-24 10:38:17 -0500

    finish up

--- overlord/lib/overlord/server.ex
+++ overlord/lib/overlord/server.ex
@@ -2,7 +2,7 @@ defmodule Overlord.Server do
   use GenServer
   alias Overlord.Game
 
-  def init(answer), do: {:ok, Game.new(answer) |> IO.inspect }
+  def init(answer), do: {:ok, Game.new(answer) }
 
   def start_link({answer, player}), do: GenServer.start_link(__MODULE__, answer, name: player)
 
